78f2cac8c26f3dddeb949d6abb98553ffcf405aa
daliataie <89037651+daliataie@users.noreply.github.com>
2022-01-21 14:21:03 -0600

    Update tomcatDeploy.groovy

--- tomcatDeploy.groovy
+++ tomcatDeploy.groovy
@@ -1,1 +1,7 @@
-
+def call (credId,tomcatId,tomcatUser)
+sshagent([credId]) {
+               sh"""
+            scp -o StrictHostKeyChecking=no target/myweb*.war ${tomcatUser}@${tomcatId}/opt/tomcat8/webapps/myweb.war
+            ssh ${tomcatUser}r@${tomcatId} /opt/tomcat8/bin/shutdown.sh
+            ssh ${tomcatUser}${tomcatId} /opt/tomcat8/bin/startup.sh
+            """
